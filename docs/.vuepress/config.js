c4703bd8361f2c81a4ebcfe68f48c396ac84f3e0
bruceyuj <444048170@qq.com>
2020-06-06 21:24:56 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,28 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around',
-    base: '/frontend-leetcode/'
+    title: 'front-end leetcode',
+    description: 'a repo about leetcode problems',
+    base: '/frontend-leetcode/',
+    themeConfig: {
+      displayAllHeaders: true,
+      sidebar: [
+        '/leetcode/'
+      ],
+      repo: 'https://github.com/BruceYuj/frontend-leetcode',
+      repoLabel: '在 github 上编辑此页',
+      docsDir: 'docs',
+      docsBranch: 'master',
+      editLinks: true,
+      editLinkText: '帮助我改善此页面'
+    },
+    plugins: [
+      [
+        'vuepress-plugin-mathjax',
+        {
+          targeet: 'svg',
+          macros: {
+            '*': '\\times',
+          }
+        }
+      ]
+    ]
   }
