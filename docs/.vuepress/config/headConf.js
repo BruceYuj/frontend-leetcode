502018626707e951999a30222fb0eafcbbb1aaec
Bruce <444048170@qq.com>
2020-06-16 16:21:14 +0800

    add leetcode 1477

--- docs/.vuepress/config/headConf.js
+++ docs/.vuepress/config/headConf.js
@@ -1,7 +1,7 @@
 
 module.exports = [
-  ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
-  ['link', { rel: 'icon', href: '/favicon.ico' }],
-  ['link', { rel: 'manifest', href: '/manifest.json' }],
+  // ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+  // ['link', { rel: 'icon', href: '/favicon.ico' }],
+  // ['link', { rel: 'manifest', href: '/manifest.json' }],
   ['meta', { name: 'theme-color', content: '#ffffff' }],
 ];
